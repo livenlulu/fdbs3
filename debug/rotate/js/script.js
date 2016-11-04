1c4cbeecbe4b8a63e59c557ecfb41959574e77b9
Lucy Kang <lucykang@nyu.edu>
2016-11-03 20:29:04 -0700

    a

--- debug/rotate/js/script.js
+++ debug/rotate/js/script.js
@@ -586,17 +586,4 @@ $("#map").on('click', function(f) {
 });
 
 
-angular.module('plunker', ['ui.bootstrap']);
-function rig($scope) {
-
-  $scope.status = {
-    isopen: false
-  };
-
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
-  };
-}
 
